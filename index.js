db20e9dab0684d2cf79a91d867294226b0d558e9
Daniel Dawson <dannyldawson@gmail.com>
2018-11-08 07:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function subtract(a,b) {
 function multiply(a, b) {
   return a * b
 }
+
+function divide(a, b) {
+  return a/b
+}
+
+function inc(n) {
+  return n++
+}
+
+function dec(n) {
+  n--
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
